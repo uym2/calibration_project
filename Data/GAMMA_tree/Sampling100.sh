6fc764d07b771b14a6da638674bc331a32e29b22
uym2 <ceciliamaitouyen@gmail.com>
2018-05-13 16:52:44 -0700

    sample sequences

--- Data/GAMMA_tree/Sampling100.sh
+++ Data/GAMMA_tree/Sampling100.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-../../scripts_and_pipelines/full_pipeline.sh FAVITE_output/merged_tree_0.time.lb.tre FAVITE_output/merged_tree_0.lb.tre 100 20 Sampling100
+../../scripts_and_pipelines/full_pipeline.sh FAVITE_output/merged_tree_0.time.lb.tre FAVITE_output/merged_tree_0.lb.tre FAVITE_output/sequence_data.fasta 100 20 Sampling100
